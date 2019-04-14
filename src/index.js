18ab21d1fe28f5f3a4b4e5bef9192e56586589c9
ryanboris <ryan.boris@gmail.com>
2019-04-14 03:28:24 -0400

    :construction: adds webpack deps and root index

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Index = () => <h1>Welcome to my React Boilerplate</h1>
+
+ReactDOM.render(<Index />, document.getElementById('root'))
